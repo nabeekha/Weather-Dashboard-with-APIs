40d5df4227bb7581d5537ec59c8f86e0d6b11b47
nabeekha <nabeekha@umich.edu>
2023-03-03 21:11:24 -0500

    add buttons and stylistic elements

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
+var buttonEl = document.getElementById("search-button")
+
+fetch('api.openweathermap.org/data/2.5/forecast?lat={40.71}&lon={-74.01}&appid={}')
+.then(response => {
+    return response.json();
+})
+
 //create a form element to search for a city
+
+//add event listener for search button click
+buttonEl.addEventListener('click', function() {
+    console.log("test");
+})
 
 //create conditional statements to pick specific API call portions based on what city is searched
 
 //use localstorage elements to store the search history
 
 //create event listener for search history button 
+
 //use event listener to pull the api history for weather again

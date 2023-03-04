8cb97e03a613c3ab23ab99fe5a27e137ed6355ae
nabeekha <nabeekha@umich.edu>
2023-03-03 19:37:56 -0500

    initial background app build

---
+++ script.js
@@ -0,0 +1,8 @@
+//create a form element to search for a city
+
+//create conditional statements to pick specific API call portions based on what city is searched
+
+//use localstorage elements to store the search history
+
+//create event listener for search history button 
+//use event listener to pull the api history for weather again
